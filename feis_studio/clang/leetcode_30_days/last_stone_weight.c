aa71e4f7f3c8f83bb62daaae8c32c05e73bf204f
edwardyi <yi.wei.jie@hotmail.com>
2021-11-19 18:02:38 +0800

    feat(last_stone_weight): extract by counter and qsort

--- feis_studio/clang/leetcode_30_days/last_stone_weight.c
+++ feis_studio/clang/leetcode_30_days/last_stone_weight.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 int extractMaxData(int* data, int stoneSize) {
 
@@ -43,6 +44,24 @@ int extractMaxDataByIndex(int* stones, int stoneSize) {
     return maxValue;
 }
 
+int extractMaxByCounter(int* counter, int upperBound) {
+    
+    // 1000
+    for (int i=upperBound; i>0; i--) {
+        if (counter[i] > 0) {
+            // substract 1 to remove array
+            counter[i] = counter[i]-1;
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+void insertByCounter(int* counter, int value) {
+    counter[value] = counter[value] + 1;
+}
+
 void insert(int* stones, int stoneSizes, int value) {
     for (int i=0; i<stoneSizes; i++) {
         if (stones[i] == 0) {
@@ -52,19 +71,54 @@ void insert(int* stones, int stoneSizes, int value) {
     }
 }
 
+int cmp (const void *a, const void *b) {
+    return *(const int*)b - *(const int*)a;
+}
+
+
 int lastStoneWeight(int* stones, int stoneSize) {
+
+    if (stoneSize < 2) {
+        return stones[0];
+    }
+
+    int counter[1001] = {0};
+
+    // stone number in counter
+    for (int i = 0; i<stoneSize; i++) {
+        counter[stones[i]] = counter[stones[i]]+1;
+    }
+
+    int upperBound = 1000;
 
     while(true) {
         // x is greater than y
         // int x = extractMaxData(stones, stoneSize);
         // int y = extractMaxData(stones, stoneSize);
 
-        int x = extractMaxDataByIndex(stones, stoneSize);
-        int y = extractMaxDataByIndex(stones, stoneSize);
+        // int x = extractMaxDataByIndex(stones, stoneSize);
+        // int y = extractMaxDataByIndex(stones, stoneSize);
+
+        int x = extractMaxByCounter(counter, upperBound);
+        int y = extractMaxByCounter(counter, upperBound);
+
+        // printf("%d %d", x, y);
+        // return 0;
+
+        // time: O(nlogn)
+        /**
+        qsort(stones, stoneSize, sizeof(int), cmp);
+
+        int x = stones[0];
+        int y = stones[1];
+
+        // qsort assign stones zero to avoid infinity loop
+        stones[0] = stones[1] = 0;
 
         for (int i=0; i<stoneSize; i++) {
             printf("%d ", stones[i]);
         }
+        */
 
         printf("-----\n");
 
@@ -77,7 +131,8 @@ int lastStoneWeight(int* stones, int stoneSize) {
         // x is greater than y
         if (x != y) {
             // x-y > 0 ensure no negative number to insert to array
-            insert(stones, stoneSize,  x-y);
+            // insert(stones, stoneSize,  x-y);
+            insertByCounter(counter, x-y);
         }
     }
 
